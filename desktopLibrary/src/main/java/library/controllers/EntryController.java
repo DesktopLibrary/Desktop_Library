7227f6b7f3aaad9d8ed68c7c43024c7101aa480c
StoyanTodorinov <s.todorinov@yahoo.com>
2018-01-11 21:11:59 +0200

    Adding Entry implementation

--- desktopLibrary/src/main/java/library/controllers/EntryController.java
+++ desktopLibrary/src/main/java/library/controllers/EntryController.java
@@ -1,4 +1,27 @@
 package library.controllers;
 
-public class EntryController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
+
+public class EntryController{
+
+    @FXML
+    private GridPane rootPane;
+
+    @FXML
+    public void loginButtonClicked() throws IOException {
+        AnchorPane loginScene = FXMLLoader.load(getClass().getResource("/FXML/login.fxml"));
+        this.rootPane.getChildren().setAll(loginScene);
+    }
+
+    @FXML
+    public void registerButtonClicked() throws IOException {
+        AnchorPane registerScene = FXMLLoader.load(getClass().getResource("/FXML/register.fxml"));
+        this.rootPane.getChildren().setAll(registerScene);
+    }
 }
