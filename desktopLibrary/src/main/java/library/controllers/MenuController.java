5719355c07affc436c18907a1c09725459f771ee
StoyanTodorinov <s.todorinov@yahoo.com>
2018-01-11 22:35:42 +0200

    Adding Menu controller

---
+++ desktopLibrary/src/main/java/library/controllers/MenuController.java
@@ -0,0 +1,72 @@
+package library.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import library.entities.User;
+import library.services.api.BookService;
+import library.services.api.UserService;
+import library.services.impl.BookServiceImpl;
+import library.services.impl.UserServiceImpl;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+
+    private UserService userService;
+    private BookService bookService;
+    private User user;
+
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private Label loggedName;
+
+    @FXML
+    public void viewBooksClicked() throws IOException {
+        FXMLLoader booksLoader = new FXMLLoader(getClass().getResource("/FXML/userBooks.fxml"));
+
+        AnchorPane root = booksLoader.load();
+        UserBooksController controller = booksLoader.<UserBooksController>getController();
+        controller.initData(user);
+
+        this.rootPane.getChildren().setAll(root);
+    }
+
+    @FXML
+    public void editProfileClicked() {
+
+    }
+
+    @FXML
+    public void addBookClicked() throws IOException {
+        FXMLLoader addBookLoader = new FXMLLoader(getClass().getResource("/FXML/addBook.fxml"));
+
+        AnchorPane root = addBookLoader.load();
+        AddBookController controller = addBookLoader.<AddBookController>getController();
+        controller.initData(user);
+
+        this.rootPane.getChildren().setAll(root);
+    }
+
+    @FXML
+    void exitClicked() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.bookService = new BookServiceImpl();
+        this.userService = new UserServiceImpl();
+
+    }
+
+    public  void initData(User user){
+        this.user = user;
+        this.loggedName.setText("Hello, " + this.user.getUsername());
+    }
+}
